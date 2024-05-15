fed22a205e7656966b24340299e195e8dcb0bd53
Jonathan Saussereau <jsaussereau@protonmail.com>
2024-05-15 11:24:34 +0200

    Add license header

--- simulation/verilator/src/tb_soc_wrapper_top.cpp
+++ simulation/verilator/src/tb_soc_wrapper_top.cpp
@@ -1,3 +1,25 @@
+/**********************************************************************\
+*                               AsteRISC                               *
+************************************************************************
+*
+* Copyright (C) 2022 Jonathan Saussereau
+*
+* This file is part of AsteRISC.
+* AsteRISC is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* AsteRISC is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with AsteRISC. If not, see <https://www.gnu.org/licenses/>.
+*
+*/
+
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include "include_all.h"
