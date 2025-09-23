b5cd632ea49d24d49b6ea88051e632e3d92e0b32
Jonathan Saussereau <jsaussereau@protonmail.com>
2025-09-23 14:35:29 +0200

    Add copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "sphinx_copybutton",
     'sphinx_tabs.tabs',
 #    'sphinx_inline_tabs',
 ]
